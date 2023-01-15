8e2b2b1b7fbd4455cb8120b05c84beee974c8d34
Koji Onishi <fursich0@gmail.com>
2023-01-15 16:44:12 +0900

    factor out loading logics from loader struct

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ where
 {
     filename: String,
     base_dir: Option<String>,
-    record_map: Option<Dict<T>>,
+    named_records: Option<Dict<T>>,
 }
 
 pub type Dict<T> = HashMap<String, T>;
@@ -31,52 +31,28 @@ where
         Self {
             filename: filename.to_string(),
             base_dir: base_dir.map(|dir| dir.to_string()),
-            record_map: None,
+            named_records: None,
         }
     }
 
     pub fn load(&mut self, dependencies: &Dict<String>) -> Result<&Self> {
         info!("loading {}...", self.filename);
 
-        if self.record_map.is_some() {
+        if self.named_records.is_some() {
             return Err(anyhow::anyhow!(
                 "filename : {} the records have been loaded already",
                 self.filename,
             ));
         }
 
-        // read contents as string from the seed file
-        let raw_text = read_file(&self.filename, self.base_dir.as_deref())?;
-
-        // replace embedded tags before deserialization gets started
-        let parsed_text = resolve_tags(&raw_text, dependencies).map_err(|err| {
-            anyhow::anyhow!(
-                "failed to pre-process embedded tags: {}\n   err: {}",
-                self.filename,
-                err
-            )
-        })?;
-
-        // deserialization
-        // currently accepts yaml format only, but this could accept any other serde-compatible format, e.g. json
-        let recoards = serde_yaml::from_str(&parsed_text).map_err(|err| {
-            anyhow::anyhow!(
-                "deserialization failed. check the file: {}
-            err: {}",
-                self.filename,
-                err
-            )
-        })?;
-        self.set_recoards(recoards)?;
+        let records =
+            load_named_records::<T>(&self.filename, self.base_dir.as_deref(), dependencies)?;
+        self.set_recoards(records)?;
 
         Ok(self)
     }
 
-    pub fn get_all_records(&self) -> Result<&Dict<T>> {
-        self.get_records()
-    }
-
-    pub fn get_record(&self, key: &str) -> Result<&T> {
+    pub fn get(&self, key: &str) -> Result<&T> {
         let records = self.get_records()?;
         records.get(key).ok_or_else(|| {
             anyhow::anyhow!(
@@ -87,24 +63,58 @@ where
         })
     }
 
-    fn set_recoards(&mut self, record_map: Dict<T>) -> Result<()> {
-        if self.record_map.is_some() {
+    fn set_recoards(&mut self, named_records: Dict<T>) -> Result<()> {
+        if self.named_records.is_some() {
             return Err(anyhow::anyhow!(
                 "filename : {} the records have been loaded already",
                 self.filename,
             ));
         }
 
-        self.record_map = Some(record_map);
+        self.named_records = Some(named_records);
         Ok(())
     }
 
     fn get_records(&self) -> Result<&Dict<T>> {
-        self.record_map.as_ref().ok_or_else(|| {
+        self.named_records.as_ref().ok_or_else(|| {
             anyhow::anyhow!(
                 "filename : {} no records have been loaded yet",
                 self.filename,
             )
         })
     }
+}
+
+fn load_named_records<T>(
+    filename: &str,
+    base_dir: Option<&str>,
+    dependencies: &Dict<String>,
+) -> Result<Dict<T>>
+where
+    T: DeserializeOwned,
+{
+    // read contents as string from the seed file
+    let raw_text = read_file(filename, base_dir)?;
+
+    // replace embedded tags before deserialization gets started
+    let parsed_text = resolve_tags(&raw_text, dependencies).map_err(|err| {
+        anyhow::anyhow!(
+            "failed to pre-process embedded tags: {}\n   err: {}",
+            filename,
+            err
+        )
+    })?;
+
+    // deserialization
+    // currently accepts yaml format only, but this could accept any other serde-compatible format, e.g. json
+    let records = serde_yaml::from_str(&parsed_text).map_err(|err| {
+        anyhow::anyhow!(
+            "deserialization failed. check the file: {}
+            err: {}",
+            filename,
+            err
+        )
+    })?;
+
+    Ok(records)
 }
