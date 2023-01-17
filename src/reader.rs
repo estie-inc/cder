d81c3f9ea25852a6f8ddd668718f75377df86349
Koji Onishi <fursich0@gmail.com>
2023-01-18 02:21:51 +0900

    interpret base_dir as relative to the manifest directory

--- src/reader.rs
+++ src/reader.rs
@@ -1,9 +1,13 @@
 use anyhow::Result;
-use std::{fs, path::PathBuf};
+use std::{env, fs, path::PathBuf};
 
 /// Read seeds from specified file
 pub fn read_file(filename: &str, base_dir: &str) -> Result<String> {
-    let mut path = PathBuf::from(base_dir);
+    let mut path = PathBuf::new();
+    if let Ok(current_base) = env::var("CARGO_MANIFEST_DIR") {
+        path.push(current_base)
+    }
+    path.push(base_dir);
     path.push(filename);
 
     fs::read_to_string(&path)
