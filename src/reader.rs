9f66a85e8b9d7f34d1ef0b39ecb816e082c67db2
Konippi <konippi-114@outlook.jp>
2024-08-01 22:22:29 +0900

    refactor: how to build path in reader

--- src/reader.rs
+++ src/reader.rs
@@ -3,12 +3,11 @@ use std::{env, fs, path::PathBuf};
 
 /// Read seeds from specified file
 pub fn read_file(filename: &str, base_dir: &str) -> Result<String> {
-    let mut path = PathBuf::new();
-    if let Ok(current_base) = env::var("CARGO_MANIFEST_DIR") {
-        path.push(current_base)
-    }
-    path.push(base_dir);
-    path.push(filename);
+    let path = env::var("CARGO_MANIFEST_DIR")
+        .map(PathBuf::from)
+        .unwrap_or_default()
+        .join(base_dir)
+        .join(filename);
 
     fs::read_to_string(&path)
         .map_err(|err| anyhow::anyhow!("Can't open the file: {:?}\n   err: {}", path, err))
